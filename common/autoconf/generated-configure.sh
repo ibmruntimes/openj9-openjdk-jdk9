fda2f518181febcbb0ce3bc3a8c522edeb9c0d8d
James Kingdon <jkingdon@ca.ibm.com>
2018-01-27 15:54:40 -0500

    Remove duplicate generated_target_rules
    Instead of duplicating the entire generated_target_rules section, define two
    new variables that contain the extra libraries needed when cross-compiling.
    These variables can be used within the generated_target_rules to make sure
    the jvm/jre is correctly composed.
    
    Signed-off-by: James Kingdon <jkingdon@ca.ibm.com>

--- common/autoconf/generated-configure.sh
+++ common/autoconf/generated-configure.sh
@@ -5198,11 +5198,7 @@ VS_SDK_PLATFORM_NAME_2013=
 #CUSTOM_AUTOCONF_INCLUDE
 
 # Do not change or remove the following line, it is needed for consistency checks:
-<<<<<<< a41d5ef5633e6f6ffc637fe350cd883dea93fb9b
-DATE_WHEN_GENERATED=1515058356
-=======
-DATE_WHEN_GENERATED=1512647666
->>>>>>> Enable cross-compile for armhf
+DATE_WHEN_GENERATED=1517096419
 
 ###############################################################################
 #
