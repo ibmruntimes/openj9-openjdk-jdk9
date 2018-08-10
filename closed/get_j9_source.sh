601c9abfd85cff74227100c48d0910120f45b71b
Dan Heidinga <daniel_heidinga@ca.ibm.com>
2018-08-10 10:09:51 -0400

    Update get sources to pull the openj9-0.9.0 final release tag
    Signed-off-by: Dan Heidinga <daniel_heidinga@ca.ibm.com>

--- closed/get_j9_source.sh
+++ closed/get_j9_source.sh
@@ -51,10 +51,10 @@ declare -A git_urls
 declare -A shas
 
 git_urls[openj9]=https://github.com/eclipse/openj9
-branches[openj9]=openj9-0.9.0-rc2
+branches[openj9]=openj9-0.9.0
 
 git_urls[omr]=https://github.com/eclipse/openj9-omr
-branches[omr]=openj9-0.9.0-rc2
+branches[omr]=openj9-0.9.0
 
 pflag=false
 
